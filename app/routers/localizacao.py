d3d6cbc76579160d71bb4f77f0a225a3d21b9b44
Artiom Gusanu <artyy.gus@gmail.com>
2025-12-04 10:10:25 +0000

    localizacao update

--- app/routers/localizacao.py
+++ app/routers/localizacao.py
@@ -1,12 +1,8 @@
 from fastapi import APIRouter, HTTPException
 from app.db import get_connection
-<<<<<<< HEAD
-=======
 from app.models.schemas import ResultadoFreguesia  # Define um esquema adequado para a resposta
->>>>>>> c79da8bb7610e4acf056fd82a9fd0a6604965807
 from fastapi.responses import JSONResponse
 from psycopg2.extras import RealDictCursor
-<<<<<<< HEAD
 import traceback
 
 router = APIRouter(prefix="/api")
@@ -14,11 +10,6 @@ router = APIRouter(prefix="/api")
 # ------------------------------
 # 1. Obter todos os distritos
 # ------------------------------
-=======
-
-router = APIRouter(prefix="/api")
-
->>>>>>> c79da8bb7610e4acf056fd82a9fd0a6604965807
 @router.get("/distritos")
 def obter_distritos():
     try:
@@ -37,6 +28,9 @@ def obter_distritos():
         traceback.print_exc()  # Exibe o erro no log
         return JSONResponse(status_code=500, content={"detail": str(e)})
 
+# ------------------------------
+# 2. Obter todos os municípios de um distrito
+# ------------------------------
 @router.get("/municipios")
 def obter_municipios(distrito: str):
     try:
@@ -61,6 +55,9 @@ def obter_municipios(distrito: str):
         traceback.print_exc()  # Exibe o erro no log
         return JSONResponse(status_code=500, content={"detail": str(e)})
 
+# ------------------------------
+# 3. Obter todas as freguesias de um município
+# ------------------------------
 @router.get("/freguesias")
 def obter_freguesias(municipio: str):
     try:
@@ -85,84 +82,9 @@ def obter_freguesias(municipio: str):
         traceback.print_exc()  # Exibe o erro no log
         return JSONResponse(status_code=500, content={"detail": str(e)})
 
-
-# @router.get("/obter_freguesia")
-# def obter_freguesia(lat: float, lon: float):
-#     try:
-#         # Estabelece a conexão com o banco de dados
-#         conn = get_connection()
-#         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-#             
-#             # Consulta para obter a freguesia pela coordenada (lat, lon)
-#             cur.execute("""
-#                 SELECT con_name
-#                 FROM area_freguesias
-#                 WHERE ST_Contains(
-#                     geom,
-#                     ST_SetSRID(ST_Point(%s, %s), 4326)
-#                 )
-#                 LIMIT 1;
-#             """, (lon, lat))
-#             
-#             freguesia = cur.fetchone()
-# 
-#             if not freguesia:
-#                 return JSONResponse(status_code=404, content={"detail": "Freguesia não encontrada"})
-# 
-#             nome_freguesia = freguesia["con_name"]
-# 
-#             # Agora, vamos obter dados sobre o município relacionado à freguesia
-#             cur.execute("""
-#                 SELECT municipio, ST_AsGeoJSON(geom) AS geom
-#                 FROM cont_municipios
-#                 WHERE municipio = %s;
-#             """, (nome_freguesia,))
-# 
-#             municipio_geom = cur.fetchone()
-# 
-#             if not municipio_geom:
-#                 return JSONResponse(status_code=404, content={"detail": "Município não encontrado"})
-# 
-#             # Extraindo a geometria (em formato GeoJSON) do município
-#             geom_municipio = municipio_geom["geom"]
-#             nome_municipio = municipio_geom["municipio"]
-# 
-#             # Agora, obtemos os indicadores relacionados ao município
-#             cur.execute("""
-#                 SELECT c.descr_concelho, 
-#                        i.*,
-#                        te.numero_total,
-#                        te.numero_nao_transicao,
-#                        ROUND(te.taxa_nao_transicao, 2) AS taxa_nao_transicao,
-#                        te.numero_transicao,
-#                        ROUND(te.taxa_transicao, 2) AS taxa_transicao,
-#                        ie.*
-#                 FROM indicadoresconcelho i
-#                 JOIN concelho c ON i.concelho_id = c.id_concelho
-#                 LEFT JOIN transicoes_ensino te ON te.concelho_id = i.concelho_id
-#                 LEFT JOIN indicadoresempresas ie ON ie.concelho_id = i.concelho_id
-#                 WHERE c.descr_concelho = %s;
-#             """, (nome_municipio,))
-# 
-#             indicadores = cur.fetchone()
-# 
-#             if not indicadores:
-#                 return JSONResponse(status_code=404, content={"detail": "Indicadores não encontrados para o município"})
-# 
-#             # Combina a geometria do município com os dados da freguesia e indicadores
-#             response_data = {
-#                 "freguesia": nome_freguesia,
-#                 "municipio": nome_municipio,
-#                 "geom_municipio": geom_municipio,  # Geometria em formato GeoJSON
-#                 "indicadores": indicadores
-#             }
-# 
-#             return response_data
-# 
-#     except Exception as e:
-#         traceback.print_exc()  # Aparece no terminal/log
-#         return JSONResponse(status_code=500, content={"detail": str(e)})
-
+# ------------------------------
+# 4. Converter coordenadas (lat, lon) para freguesia
+# ------------------------------
 @router.get("/converter-freguesia")
 def converter_freguesia(lat: float, lon: float):
     try:
@@ -172,118 +94,6 @@ def converter_freguesia(lat: float, lon: float):
             
             # Consulta para obter a freguesia pela coordenada (lat, lon)
             cur.execute("""
-<<<<<<< HEAD
-                SELECT id_distrito, descr_distrito 
-                FROM distrito
-                ORDER BY descr_distrito;
-            """)
-            return cur.fetchall()
-    except Exception as e:
-        traceback.print_exc()
-        return JSONResponse(status_code=500, content={"detail": str(e)})
-
-# ------------------------------
-# 2. Obter todos os concelhos de um distrito
-# ------------------------------
-@router.get("/concelhos/{id_distrito}")
-def obter_concelhos(id_distrito: str):
-    try:
-        conn = get_connection()
-        with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute("""
-                SELECT id_concelho, descr_concelho
-                FROM concelho
-                WHERE distrito_id = %s
-                ORDER BY descr_concelho;
-            """, (id_distrito,))
-            return cur.fetchall()
-    except Exception as e:
-        traceback.print_exc()
-        return JSONResponse(status_code=500, content={"detail": str(e)})
-
-# ------------------------------
-# 3. Obter todas as freguesias de um concelho
-# ------------------------------
-@router.get("/freguesias/{id_concelho}")
-def obter_freguesias(id_concelho: str):
-    try:
-        conn = get_connection()
-        with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute("""
-                SELECT id_freguesia, descr_freguesia
-                FROM freguesia
-                WHERE concelho_id = %s
-                ORDER BY descr_freguesia;
-            """, (id_concelho,))
-            return cur.fetchall()
-    except Exception as e:
-        traceback.print_exc()
-        return JSONResponse(status_code=500, content={"detail": str(e)})
-
-
-
-
-
-
-
-
-
-# from fastapi import APIRouter, HTTPException
-# from app.db import get_connection
-# from app.models.schemas import ResultadoFreguesia
-# from fastapi.responses import JSONResponse
-# import traceback
-# from psycopg2.extras import RealDictCursor
-# 
-# 
-# router = APIRouter(prefix="/api")
-# 
-# @router.get("/freguesia")
-# def obter_freguesia(lat: float, lon: float):
-#     try:
-#         conn = get_connection()
-#         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-#             cur.execute("""
-#                 SELECT con_name
-#                 FROM area_freguesias
-#                 WHERE ST_Contains(
-#                     geom,
-#                     ST_SetSRID(ST_Point(%s, %s), 4326)
-#                 )
-#                 LIMIT 1;
-#             """, (lon, lat)), 
-#             
-#             freguesia = cur.fetchone()
-#             
-# 
-#             nome_concelho = freguesia["con_name"]
-# 
-#             cur.execute("""
-#                 SELECT c.descr_concelho, 
-#                        i.*,
-#                        te.numero_total,
-#                        te.numero_nao_transicao,
-#                        ROUND(te.taxa_nao_transicao, 2) AS taxa_nao_transicao,
-#                        te.numero_transicao,
-#                        ROUND(te.taxa_transicao, 2) AS taxa_transicao,
-#                         ie.*
-#                 FROM indicadoresconcelho i
-#                 JOIN concelho c ON i.concelho_id = c.id_concelho
-#                 LEFT JOIN transicoes_ensino te ON te.concelho_id = i.concelho_id
-#                 left join indicadoresempresas ie on ie.concelho_id = i.concelho_id
-#                 WHERE c.descr_concelho = %s;
-#             """, (nome_concelho,))
-# 
-#             indicadores = cur.fetchone()
-#             if not indicadores:
-#                 return JSONResponse(status_code=404, content={"detail": "Indicadores não encontrados"})
-#             
-#             return indicadores
-# 
-#     except Exception as e:
-#         traceback.print_exc()  # Aparece no terminal/log
-#         return JSONResponse(status_code=500, content={"detail": str(e)})
-=======
                 SELECT con_name
                 FROM area_freguesias
                 WHERE ST_Contains(
@@ -305,7 +115,10 @@ def obter_freguesias(id_concelho: str):
     except Exception as e:
         traceback.print_exc()  # Aparece no terminal/log
         return JSONResponse(status_code=500, content={"detail": str(e)})
-    
+
+# ------------------------------
+# 5. Obter dados estatísticos da região com base na freguesia
+# ------------------------------
 @router.get("/regionStats")
 def region_stats(freguesia: str):
     try:
@@ -362,4 +175,4 @@ def region_stats(freguesia: str):
     except Exception as e:
         traceback.print_exc()  # Aparece no terminal/log
         return JSONResponse(status_code=500, content={"detail": str(e)})
->>>>>>> c79da8bb7610e4acf056fd82a9fd0a6604965807
+
